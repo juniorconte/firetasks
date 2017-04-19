ee6f0cffaf1b8711b64481c65a33f2266f27b1b7
Junior Conte <juniorcnt@gmail.com>
2017-04-19 00:23:22 -0300

    First commit

---
+++ app/scripts/app.js
@@ -0,0 +1,27 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name todoApp
+ * @description
+ * # todoApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('todoApp', [
+    'ngAnimate',
+    'ngRoute',
+    'firebase'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,39 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name todoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the todoApp
+ */
+angular.module('todoApp')
+  .controller('MainCtrl', function ($scope, $firebaseArray) {
+
+    var ref = firebase.database().ref().child('todos');
+
+    $scope.hide = false;
+    $scope.todos = $firebaseArray(ref);
+
+    $scope.addTodo = function(title) {
+      $scope.todos.$add({
+        done: false,
+        title: title
+      });
+    };
+
+    $scope.progress = function() {
+      return $scope.todos.filter(function(todo) {
+        return todo.done;
+      }).length / $scope.todos.length * 100;
+    };
+
+    $scope.clean = function() {
+      $scope.todos.forEach(function(todo) {
+        if (todo.done) {
+          $scope.todos.$remove(todo);
+        }
+      });
+    };
+
+  });
